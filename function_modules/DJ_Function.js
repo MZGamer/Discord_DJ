0b3a6f613a128e60f514595460e7ebf5d72d1fb8
MZGamer <58198679+MZGamer@users.noreply.github.com>
2021-04-27 20:46:45 +0800

    Create DJ_Function.js

---
+++ function_modules/DJ_Function.js
@@ -0,0 +1,22 @@
+class MusicData {
+    constructor(method, title, url) {
+        this._method = method;
+        this._title = title;
+        this._url = url;
+    }
+    get method() {
+        return this._method;
+    }
+    get title() {
+        return this._title;
+    }
+    get url() {
+        return this._url;
+    }
+
+}
+class DJ {
+    constructor();
+    
+}
+module.exports;
